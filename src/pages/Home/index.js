26cab47f32d9cd65e628df99c53df4b9645eb083
Andre Alves <andrealvesaraujo.1995@gmail.com>
2022-06-30 11:00:55 -0300

    Criando nossa home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,31 @@
+import {useEffect, useState} from 'react'
+import api from '../../services/api'
+
 function Home() {
+
+    const [filmes, setFilmes] = useState([])
+
+    useEffect(()=>{
+        async function loadFilmes(){
+            const response = await api.get("movie/now_playing", {
+                params: {
+                    api_key: "c92e461fbbc78105488f14ee8e52a299",
+                    language: 'pt-BR',
+                    page: 1,
+                }
+            })
+
+            console.log(response.data.results)
+        }
+
+        loadFilmes()
+
+    }, [])
+
     return (
-        <h1>Bem vindo a home</h1>
+        <div>
+            <h1>Bem vindo a home</h1>
+        </div>
     )
 }
 
