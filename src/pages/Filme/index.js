2094b052f105c3c0bb9c0f1d85123c005b91ee4d
Andre Alves <andrealvesaraujo.1995@gmail.com>
2022-06-30 18:41:54 -0300

    Detalhes do filme

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,6 +1,55 @@
+import {useEffect, useState} from 'react'
+import {useParams} from "react-router-dom"
+
+import api from '../../services/api'
+
 function Filme() {
+
+    const { id } = useParams()
+    const [filme, setFilme] = useState({})
+    const [loading, setLoading] = useState(true)
+
+    useEffect(()=>{
+        async function loadFilmes(){
+            await api.get(`/movie/${id}`, {
+                params: {
+                    api_key: "c92e461fbbc78105488f14ee8e52a299",
+                    language: 'pt-BR',
+                }
+            })
+            .then((response)=>{
+                setFilme(response.data)
+                setLoading(false)
+            })
+            .catch(()=>{
+                console.log("Filme Não Encontrado")
+            })
+        }
+
+        loadFilmes()
+
+        return () =>{
+            console.log("Componente foi desmontado")
+        }
+    },[])
+
+    if(loading){
+        return (
+            <div className='filme-info'>
+                <h1>Carregando detalhes...</h1>
+            </div>
+        )
+    }
+
     return (
-        <h1>Bem vindo a paginas detalhes do filme</h1>
+        <div className='filme-info'>
+            <h1>{filme.title}</h1>
+            <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title} />
+        
+            <h3>Sinopse</h3>
+            <span>{filme.overview}</span>
+            <strong>Avaliação: {filme.vote_average}</strong>
+        </div>
     )
 }
 
