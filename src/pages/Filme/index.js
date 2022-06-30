adb0d046597581b77211874eef97d0cf380fafa3
Andre Alves <andrealvesaraujo.1995@gmail.com>
2022-06-30 18:52:30 -0300

    Detalhes do filme pt2

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,7 +1,9 @@
 import {useEffect, useState} from 'react'
 import {useParams} from "react-router-dom"
+import './filme-info.css'
 
 import api from '../../services/api'
+
 
 function Filme() {
 
@@ -48,7 +50,14 @@ function Filme() {
         
             <h3>Sinopse</h3>
             <span>{filme.overview}</span>
-            <strong>Avaliação: {filme.vote_average}</strong>
+            <strong>Avaliação: {filme.vote_average} / 10</strong>
+
+            <div className='area-buttons'>
+                <button>Salvar</button>
+                <button>
+                    <a href='#'>Trailer</a>
+                </button>
+            </div>
         </div>
     )
 }
