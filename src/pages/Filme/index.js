4f923fb63b5cc2c8cfd45dbcadcb87a7b6a301f4
Andre Alves <andrealvesaraujo.1995@gmail.com>
2022-07-01 16:29:04 -0300

    Salvando filme na lista

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -38,6 +38,24 @@ function Filme() {
         }
     },[navigate, id])
 
+    function salvarFilme(){
+        const minhaLista = localStorage.getItem("@primeflix")
+
+        let filmesSalvos = JSON.parse(minhaLista) || []
+
+        const hasFilme = filmesSalvos.some((filmeSalvo)=> filmeSalvo.id === filme.id)
+
+        if(hasFilme){
+            alert("Esse filme já está na lista")
+            return
+        }
+
+        filmesSalvos.push(filme)
+        localStorage.setItem("@primeflix", JSON.stringify(filmesSalvos))
+        alert("Filme salvo com sucesso")
+
+    }
+
     if(loading){
         return (
             <div className='filme-info'>
@@ -56,7 +74,7 @@ function Filme() {
             <strong>Avaliação: {filme.vote_average} / 10</strong>
 
             <div className='area-buttons'>
-                <button>Salvar</button>
+                <button onClick={salvarFilme}>Salvar</button>
                 <button>
                     <a target="blank" rel='external' href={`https://youtube.com/results?search_query=${filme.title} Trailer`}>
                         Trailer
